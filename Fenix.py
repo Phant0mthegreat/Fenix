47cd5785b76bf968d675509305c6f6b6bf2345be
Phant0m The Great <120599821+Phant0mthegreat@users.noreply.github.com>
2023-06-24 19:56:55 -0300

    Update Fenix.py

--- Fenix.py
+++ Fenix.py
@@ -1,30 +1,43 @@
-import URIs, os, cores as c, banners, sys
+import URIs, os, cores as c, banners, sys, time, URIs2
 from pystyle import Colorate, Colors
 try:
+ banners.carregando()
+ print('      © Phant0m The Great\n\n')
+ time.sleep(2)
  while True:
   os.system('clear')
   print(Colorate.Vertical(Colors.yellow_to_red, banners.banner1))
   print(f'''
-
-┎――――――――――――――――――――――――――――――――――――――――┒
-
-|       {Colorate.Vertical(Colors.yellow_to_red, '          FENIX')}                  |
-
-|――――――――――――――――――――――――――――――――――――――――|
-
-| [01] {c.blue}Escanear{c.white}     │ [03] {c.blue}Criador{c.white}       |
-
-| [02] {c.blue}Informações{c.white}  │ [00] {c.blue}Sair{c.white}          |
-
-└――――――――――――――――――――――――――――――――――――――――┘''')
+╔════════════════════════════════════════╗
+║                                        ║
+║       {Colorate.Vertical(Colors.yellow_to_red, '          FENIX')}                  ║
+║                                        ║
+║════════════════════════════════════════║
+║                                        ║
+║  [01] {c.blue}Escanear{c.white}    ║   [03] {c.blue}Criador{c.white}     ║
+║                                        ║
+║  [02] {c.blue}Informações{c.white} ║   [00] {c.blue}Sair{c.white}        ║
+║                                        ║
+╚════════════════════════════════════════╝''')
   ech=input(f'\nDigite sua escolha [{c.bblue}>{c.white}] ')
   os.system('clear')
   if ech=='01' or ech=='1':
     print(Colorate.Vertical(Colors.yellow_to_red, banners.banner1))
-    URIs.URIs()
+    print(f'[ {c.yellow}?{c.white} ] Você deseja visualizar os diretórios/arquivos que não foram encontrados ? ({c.green}s{c.white}/{c.red}n{c.white})')
+    sn=input(f'\n[{c.bblue}>{c.white}] ')
+    if sn=='s' or sn=='S':
+      os.system('clear')
+      print(Colorate.Vertical(Colors.yellow_to_red, banners.banner1))
+      URIs.URIs()
+    elif sn=='n' or sn=='N':
+      os.system('clear')
+      print(Colorate.Vertical(Colors.yellow_to_red, banners.banner1))
+      URIs2.URIs2()
+      
+
   elif ech=='02' or ech=='2':
     print(Colorate.Vertical(Colors.yellow_to_red, banners.banner1))
-    print(f'\n[ {c.yellow}×{c.white} ] O Fenex, é uma ferramenta de pentester, ele tem a capacidade de encontrar diretórios em sites, e páginas que podem haver alguma vulnerabilidade.')
+    print(f'\n[ {c.yellow}∆{c.white} ] O Fenex, é uma ferramenta de pentester, ele tem a capacidade de encontrar diretórios em sites, e páginas que podem haver alguma vulnerabilidade.\n\n[ {c.yellow}V{c.white} ] Versão: 2.0\n\n[ {c.yellow}×{c.white} ] Bugs: Caso tenho encontrado algum bug no Fenix, faça um Bug-Report entrando em contato com o criador.')
     print(f'\n{c.bwhite}[ENTER]{c.white} para voltar ao menu.')
     input()
   elif ech=='03' or ech=='3':
